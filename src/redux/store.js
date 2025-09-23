62912214a83077effacc97c144bca800aebd863b
Evgeniy <evgnec@gmail.com>
2025-09-23 23:52:22 +0300

    upd

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
+
+const myReducer = createReducer(0, {});
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    myValue: myReducer
+  },
 })
