b17172586e23d5ad920dd48c18350b689068316f
Evgeniy <evgnec@gmail.com>
2025-10-04 16:09:20 +0300

    upd

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { createAction, createReducer } from '@reduxjs/toolkit'
 
-const myReducer = createReducer(0, {});
+// створюємо екшени
+export const increment = createAction('myValue/increment');
+export const decrement = createAction('myValue/decrement');
+
+// редʼюсер через builder callback
+const myReducer = createReducer(0, (builder) => {
+    builder
+      .addCase(increment, (state) => state + 1)
+      .addCase(decrement, (state) => state - 1);
+  });
 
 export default configureStore({
   reducer: {
